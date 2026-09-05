49883bc5086a3d6416e13496c9e3b8697555e3d4
Guanyi Wang <guanyiwang.gyw@gmail.com>
2026-09-05 02:27:55 +0100

    Add account adding functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,75 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import "../styles/Home.css";
 function Home() {
-    return <div>Home</div>;
+    const [accounts, setAccounts] = useState([]);
+    const [balance, setBalance] = useState(0.0);
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        getAccounts();
+    }, []);
+
+    const getAccounts = () => {
+        api.get("/api/accounts/")
+            .then((res) => res.data)
+            .then((data) => {
+                (setAccounts(data), console.log(data));
+            })
+            .catch((err) => alert(err));
+    };
+
+    const deleteAccount = (id) => {
+        api.delete(`/api/accounts/delete/${id}/`)
+            .then((res) => {
+                if (res.status === 204) alert("Account was deleted");
+                else alert("Failed to delete account!");
+                getAccounts();
+            })
+            .catch((err) => alert(err));
+    };
+
+    const createAccount = (e) => {
+        e.preventDefault();
+        api.post("/api/accounts/", { balance, name })
+            .then((res) => {
+                if (res.status === 201) alert("Account added!");
+                else alert("Failed to add account");
+                getAccounts();
+            })
+            .catch((err) => alert(err));
+    };
+
+    return (
+        <div>
+            <div>
+                <h2>Accounts</h2>
+            </div>
+            <h2>Add an account</h2>
+            <form onSubmit={createAccount}>
+                <label htmlFor="name">Name:</label>
+                <br />
+                <input
+                    type="text"
+                    id="name"
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+                <label htmlFor="balance">Balance:</label>
+                <br />
+                <input
+                    type="number"
+                    id="balance"
+                    step="0.01"
+                    required
+                    onChange={(e) => setBalance(e.target.value)}
+                    value={balance}
+                />
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
+    );
 }
 
 export default Home;
