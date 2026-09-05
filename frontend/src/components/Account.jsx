7fc4c6e431d0f180181b70e224f29b54e415622a
Guanyi Wang <guanyiwang.gyw@gmail.com>
2026-09-05 19:00:33 +0100

    fix: add £ sign in front of balance

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import "../styles/Account.css";
 
 function Account({ account, onDelete }) {
-    return (
-        <div className="account-container">
-            <p className="account-name">{account.name}</p>
-            <p className="account-balance">{account.balance}</p>
-            <button
-                className="delete-button"
-                onClick={() => onDelete(account.id)}
-            >
-                Delete
-            </button>
-        </div>
-    );
+  return (
+    <div className="account-container">
+      <p className="account-name">{account.name}</p>
+      <p className="account-balance">£{account.balance}</p>
+      <button className="delete-button" onClick={() => onDelete(account.id)}>
+        Delete
+      </button>
+    </div>
+  );
 }
 
 export default Account;
