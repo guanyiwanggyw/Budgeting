471318fb73e09e252bf46c21889cf47f98c4c11a
Guanyi Wang <guanyiwang.gyw@gmail.com>
2026-09-06 19:34:23 +0100

    fix: add missing config

--- backend/budgeting/urls.py
+++ backend/budgeting/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
+from users.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls")),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/transactions/", include("transactions.urls")),
 ]
